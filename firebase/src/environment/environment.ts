93e46dbdf3e6b5bec44f116b7ea4975f58af5234
Manny <manish29polekar@gmail.com>
2025-05-13 15:28:32 +0530

    Add environment files for Vercel build

---
+++ firebase/src/environment/environment.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+    apiKey: "AIzaSyDZW887cBNO8dl6BuNLBSwyulupCVBfbmI",
+    authDomain: "angular-crud-6b5e3.firebaseapp.com",
+    projectId: "angular-crud-6b5e3",
+    storageBucket: "angular-crud-6b5e3.appspot.com",
+    messagingSenderId: "752516871967",
+    appId: "1:752516871967:web:ed90c0f32dab7954c2622a"
+  };
